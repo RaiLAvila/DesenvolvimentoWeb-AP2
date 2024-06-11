29d0ad6ab21be40ad32e890a39bec2696faa06d5
RAI LAMPER DE AVILA <202402627279@alunos.ibmec.edu.br>
2024-06-11 08:54:56 -0300

    filtros

--- codigo.js
+++ codigo.js
@@ -2,13 +2,34 @@ import { hex_sha256 } from "./sha256-min.mjs"
 
 if (localStorage.getItem('authorized')){
     document.body.innerHTML = `
-    <header style="background-color: rgb(0, 97, 60); height: 100px">
-    <h1>Botafogo AP2</h1>
-    <button id="logout">Logout</button>
+    <header style="background-color: rgb(0, 97, 60); 
+    padding: 0.5em 1em; position: relative; display: flex;
+    flex-direction: row;"><h1 style="color: white; margin: 
+    0px; flex-grow: 1;">Elenco Botafogo 2024</h1><button id="logout"
+    style="background-color: rgb(0, 97, 60); border: none; color: white;
+    text-align: center; text-decoration: none; padding: 0.5em 1em;
+    cursor: pointer;">Logout</button></header>    <h1>Filtros</h1>
     </header>
-    <h1>aloooooo</h1>
-    `  
-     document.getElementById('logout').onclick = () =>{
+    
+    <div id="barra-escolhas" style="display: flex; flex-direction: 
+    row; justify-content: center; gap: 1em; margin: 2em 0px;"><button id"masculino"
+    style="background-color: rgb(0, 97, 60); border: none;
+    color: white; text-align: center; text-decoration: none; padding: 
+    0.4em 1em; cursor: pointer; font-size: 1.5em; max-width: 6em;">
+    Masculino</button><button id="feminino" style="background-color:
+    rgb(0, 97, 60); border: none; color: white; text-align: center;
+    text-decoration: none; padding: 0.5em 1em; cursor: pointer; 
+    font-size: 1.3em; max-width: 6em;">Feminino</button><button 
+    id="todos" style="background-color: rgb(0, 97, 60); border: none; color:
+    white; text-align: center; text-decoration: none; padding: 0.5em 
+    1em; cursor: pointer; font-size: 1.3em; max-width: 6em;">Elenco
+    Completo</button></div>
+    ` 
+    document.getElementById('masculino').onclick = () =>{
+        localStorage.removeItem('authorized');
+        window.location.href= 'detalhes.html';
+     } 
+    document.getElementById('logout').onclick = () =>{
         localStorage.removeItem('authorized');
         window.location.href= 'index.html';
      }

fa0f121b0401a213f69e48e791e7d6dc44ceb38f
DESKTOPFB\Fabio <lamperdeavila@gmail.com>
2024-06-15 16:24:29 -0300

    filtro detalhes e mais

--- codigo.js
+++ codigo.js
@@ -16,26 +16,36 @@ if (localStorage.getItem('authorized')){
 
 <h1>Filtros</h1>
 
-    ` 
+` 
     document.getElementById('logout').onclick = () =>{
         localStorage.removeItem('authorized');
         window.location.href= 'index.html';
-     }
-     
-     let dados;
-     
-     const divPesquisa = document.createElement('div');
-     divPesquisa.style.textAlign = 'center';
-     divPesquisa.style.padding = '5px';
-     divPesquisa.style.cssText=`
+    }
+    
+    let dados;
+    
+    const divPesquisa = document.createElement('div');
+    divPesquisa.style.textAlign = 'center';
+    divPesquisa.style.padding = '5px';
+    divPesquisa.style.cssText=`
     text-align: center;
     margin: 0px auto 2em;
     padding: 5px;
-     `;
+    `;
+     const usarFiltro = (caminho) => {
+         pegaDados(caminho).then(
+            (entrada)=> {
+                dados = entrada;
+                conteudo.innerHTML = "";
+                dados.forEach(
+                    (atleta) => {
+                        conteudo.appendChild(montaCard(atleta))
+                    }
+                )
+            }
+        )
+     }
 
-     
-     
-     const container = document.createElement("div")
      const divBotao = document.createElement("div")
      const botaoTodos = document.createElement("button")
      const botaoFem = document.createElement("button")
@@ -50,25 +60,34 @@ if (localStorage.getItem('authorized')){
      botaoTodos.style.justifyContent = "center";
      botaoTodos.style.gap = "1.5em";
      botaoTodos.style.margin = "2.4em 0px";
-
-     botaoFem.classList.add("button");
-     botaoFem.innerHTML = "Feminino";
-     botaoFem.style.display = "flex";
-     botaoFem.style.justifyContent = "center";
-     botaoFem.style.gap = "1.5em";
-     botaoFem.style.margin = "2.4em 0px";
-
-     botaoMas.classList.add("button");
-     botaoMas.innerHTML = "Masculino";
-     botaoMas.style.display = "flex";
-     botaoMas.style.justifyContent = "center";
-     botaoMas.style.gap = "1.5em";
-     botaoMas.style.margin = "2.4em 0px";
-
-     const inputPesquisa = document.createElement('input');
-     inputPesquisa.type = 'search';
-     inputPesquisa.placeholder = "Busque o jogador"
-     inputPesquisa.style.cssText = `
+     botaoTodos.onclick = ()=>  {
+         usarFiltro("https://botafogo-atletas.mange.li/2024-1/all")
+        }
+        
+        botaoFem.classList.add("button");
+        botaoFem.innerHTML = "Feminino";
+        botaoFem.style.display = "flex";
+        botaoFem.style.justifyContent = "center";
+        botaoFem.style.gap = "1.5em";
+        botaoFem.style.margin = "2.4em 0px";
+        botaoFem.onclick = ()=>  {
+            usarFiltro("https://botafogo-atletas.mange.li/2024-1/feminino")
+        }
+        
+        botaoMas.classList.add("button");
+        botaoMas.innerHTML = "Masculino";
+        botaoMas.style.display = "flex";
+        botaoMas.style.justifyContent = "center";
+        botaoMas.style.gap = "1.5em";
+        botaoMas.style.margin = "2.4em 0px";
+        botaoMas.onclick = ()=>  {
+            usarFiltro("https://botafogo-atletas.mange.li/2024-1/masculino")
+        }
+        
+        const inputPesquisa = document.createElement('input');
+        inputPesquisa.type = 'search';
+        inputPesquisa.placeholder = "Busque o jogador"
+        inputPesquisa.style.cssText = `
      text-align: center;
      line-height: 3em;
      max-width: 80%;
@@ -80,7 +99,7 @@ if (localStorage.getItem('authorized')){
      divBotao.appendChild(botaoTodos)
      divPesquisa.appendChild(divBotao)
      divPesquisa.appendChild(inputPesquisa);
-
+     
      document.body.appendChild(divPesquisa);
      
      const conteudo = document.createElement('div');
@@ -93,7 +112,7 @@ if (localStorage.getItem('authorized')){
      
      document.body.appendChild(conteudo);
      
-     const montaCard = (entrada) => {
+    const montaCard = (entrada) => {
          const card = document.createElement('div');
          card.style.display = 'grid';
          card.style.gridTemplateColumns = '1fr 2fr';
@@ -121,7 +140,7 @@ if (localStorage.getItem('authorized')){
          imagem.style.width = '200px';
          imagem.style.height = '100%';
          imagem.style.objectFit = 'cover';
-        
+         
          const posicao = document.createElement('p');
          posicao.innerHTML = entrada.posicao;
          posicao.style.cssText = `
@@ -146,98 +165,58 @@ if (localStorage.getItem('authorized')){
          nome.style.display = 'flex';
          nome.style.gap = "5px"
          nome.style.fontWeight = 'bold';
-
+         
          const descricao = document.createElement('p');
          /*descricao.innerHTML = entrada.detalhes;*/
          descricao.style.gridArea = 'a4';
          
-        const detalhesContainer = document.createElement('div');
-        detalhesContainer.style.position = 'fixed';
-        detalhesContainer.style.top = '0';
-        detalhesContainer.style.left = '0';
-        detalhesContainer.style.width = '100%';
-        detalhesContainer.style.height = '100%';
-        detalhesContainer.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
-        detalhesContainer.style.display = 'none';
-        detalhesContainer.style.justifyContent = 'center';
-        detalhesContainer.style.alignItems = 'center';
-        detalhesContainer.style.zIndex = '9999'; 
-
-        const imagemAmpliada = document.createElement('img');
-        imagemAmpliada.style.width = '400px'; 
-        imagemAmpliada.style.height = 'auto'; 
-        imagemAmpliada.style.objectFit = 'contain'; 
-
-        const detalhesAmpliados = document.createElement('div');
-        detalhesAmpliados.style.color = '#fffff'; 
-        detalhesAmpliados.style.marginTop = '10px'; 
-        detalhesAmpliados.style.textAlign = 'center'; 
-
-        const nascimento = document.createElement('p');
-        nascimento.innerHTML = entrada.nascimento;
-        nascimento.style.gridArea = 'a5';
-        nascimento.style.top = '0';
-        nascimento.style.left = '0';
-        nascimento.style.margin = '10px';
-        nascimento.style.display = 'flex';
-        nascimento.style.gap = "5px"
-        nascimento.style.fontWeight = 'bold';
-
-        const detalhesButton = document.createElement('button');
-        detalhesButton.innerHTML = 'Detalhes';
-        detalhesButton.style.gridArea = 'a6';
-        detalhesButton.style.backgroundColor = "rgb(fffffff)";
-        detalhesButton.style.fontWeight = 'bold';
-        detalhesButton.style.textAlign = "center";
-        detalhesButton.style.cursor =  "pointer";
-        detalhesButton.style.border = 'none';
-        detalhesButton.style.padding = '1px 2px';
-        detalhesButton.style.width = "8.5rem";
-        detalhesButton.style.borderRadius = '5px';
-        detalhesButton.style.fontSize = "1.5em";
-        detalhesButton.style.border = "none";
-        detalhesButton.style.transition = "transform 0.3s";
-
-        detalhesButton.classList.add('detalhes-button');
-        detalhesButton.addEventListener('click', (event) => {
-        if (event.target === detalhesContainer) {
-        detalhesContainer.style.display = 'none';
-    }
-
-        
-        // Mostrar os detalhes ampliados
-        detalhesContainer.style.display = 'flex';
-
-        imagemAmpliada.scr = imagem;
-        detalhesAmpliados.innerHTML = `
-        <h2>${nome}</h2>
-        <p>Posição: ${posicao}</p>
-        <p>Nascimento: ${nascimento}</p>
-        <p>Descrição: ${descricao}</p>
-        `;
-
-        detalhesContainer.appendChild(imagemAmpliada);
-        detalhesContainer.appendChild(detalhesAmpliados);
-        document.body.appendChild(detalhesContainer);
-
-
-    });
-        
-        card.appendChild(imgContainer);
-        imgContainer.appendChild(imagem);
-        card.appendChild(posicao);
-        card.appendChild(nome);
-        card.appendChild(descricao);
-        card.appendChild(nascimento);
-        card.appendChild(detalhesButton);
+         const nascimento = document.createElement('p');
+         nascimento.innerHTML = entrada.nascimento;
+         nascimento.style.gridArea = 'a5';
+         nascimento.style.top = '0';
+         nascimento.style.left = '0';
+         nascimento.style.margin = '10px';
+         nascimento.style.display = 'flex';
+         nascimento.style.gap = "5px"
+         nascimento.style.fontWeight = 'bold';
          
-         return card;
-        }
+         const detalhesButton = document.createElement('button');
+         detalhesButton.innerHTML = 'Detalhes';
+         detalhesButton.style.gridArea = 'a6';
+         detalhesButton.style.backgroundColor = "rgb(fffffff)";
+         detalhesButton.style.fontWeight = 'bold';
+         detalhesButton.style.textAlign = "center";
+         detalhesButton.style.cursor =  "pointer";
+         detalhesButton.style.border = 'none';
+         detalhesButton.style.padding = '1px 2px';
+         detalhesButton.style.width = "8.5rem";
+         detalhesButton.style.borderRadius = '5px';
+         detalhesButton.style.fontSize = "1.5em";
+         detalhesButton.style.border = "none";
+         detalhesButton.style.transition = "transform 0.3s";
+         
+         detalhesButton.classList.add('detalhes-button');
+         detalhesButton.onclick = ()=> {
+             const Id = entrada.id;
+             window.location.href = `detalhes.html?id=${Id}`;
+            }
+            
+            
+            card.appendChild(imgContainer);
+            imgContainer.appendChild(imagem);
+            card.appendChild(posicao);
+            card.appendChild(nome);
+            card.appendChild(descricao);
+            card.appendChild(nascimento);
+            card.appendChild(detalhesButton);
+            
+            return card;
+        };
         
         inputPesquisa.onkeyup = (ev) => {
             console.log(ev.target.value);
             
-            if (ev.target.value.length > 3){
+            if (ev.target.value.length > 1){
                 
                 const filtrado = dados.filter(
                     (elemento) => {
@@ -245,63 +224,71 @@ if (localStorage.getItem('authorized')){
                         const estaNaPosicao = elemento.posicao.toLowerCase().includes(ev.target.value.toLowerCase())
                         return estaNoNome || estaNaPosicao
                     }
+                );
+                
+                conteudo.innerHTML = '';
+                
+                filtrado.forEach(
+                    (atleta) => (
+                        conteudo.appendChild(montaCard(atleta))
                     )
-                    
-                    conteudo.innerHTML = '';
-                    
-                    filtrado.forEach(
-                        (atleta) => (
-                            conteudo.appendChild(montaCard(atleta))
-                            )
-                            )
-    }
-}
-
-const pegaDados = async(caminho) => {
-    const resposta = await fetch(caminho);
-    const dados = await resposta.json();
-    return dados;
-}
-
-pegaDados("https://botafogo-atletas.mange.li/2024-1/all").then(
-    (entrada) => {
-        dados = entrada;
-        conteudo.innerHTML = '';
-        dados.forEach(
-            (atleta) => {
-                conteudo.appendChild(montaCard(atleta))
+                );
             }
-            )
-        });
-        
-        console.log('sincrono');
-    }
-    
-    else{
-        
-        document.body.innerHTML = `
-        <h1>Login</h1>
-        <label style= "color: white">
-        Senha:
-        <input type ="password" id="senha">
-        </label>
-        <button id="btn_login">Entrar</button>
-        <h5 style= "color: white"> a senha é 456 </h5>`
-        
-}
+            else if (ev.target.value.length === 0){
+                conteudo.innerHTML = "";
+                dados.forEach((atleta)=>(conteudo.appendChild(montaCard(atleta))
+                        )
+                    )
+                    }
+                }
+                
+                const pegaDados = async(caminho) => {
+                    const resposta = await fetch(caminho);
+                    const dados = await resposta.json();
+                    return dados;
+                }
+                
+                pegaDados("https://botafogo-atletas.mange.li/2024-1/all").then(
+                    (entrada) => {
+                        dados = entrada;
+                        conteudo.innerHTML = '';
+                        dados.forEach(
+                            (atleta) => {
+                                conteudo.appendChild(montaCard(atleta))
+                            }
+                        )
+                    });
+                    
+                    console.log('sincrono');
+                
+            }
+                
+                else{
+                    
+                    document.body.innerHTML = `
+                    <h1>Login</h1>
+                    <label style= "color: white">
+                    Senha:
+                    <input type ="password" id="senha">
+                    </label>
+                    <button id="btn_login">Entrar</button>
+                    <h5 style= "color: white"> a senha é 456 </h5>`
+                    
+                }
     document.getElementById('btn_login').onclick = () => {
         const password = document.getElementById('senha').value;
-    
-    // Calcular hash SHA-256 da senha
-    const hash = hex_sha256(password); // Supondo que você tenha uma função sha256 disponível
-
-    // Comparar o hash com o hash esperado
-    const expectedHash = 'b3a8e0e1f9ab1bfe3a36f231f676f78bb30a519d2b21e6c530c0eee8ebb4a5d0'; // Substitua pelo hash SHA-256 correto da senha
-    if (hash === expectedHash) {
-        localStorage.setItem('authorized', 'true');
-        window.location.href = 'index.html'; // Redirecionar para a página de detalhes
-    } else {
-        alert('Senha incorreta');
+        
+        // Calcular hash SHA-256 da senha
+        const hash = hex_sha256(password); // Supondo que você tenha uma função sha256 disponível
+        
+        // Comparar o hash com o hash esperado
+        const expectedHash = 'b3a8e0e1f9ab1bfe3a36f231f676f78bb30a519d2b21e6c530c0eee8ebb4a5d0'; // Substitua pelo hash SHA-256 correto da senha
+        if (hash === expectedHash) {
+            localStorage.setItem('authorized', 'true');
+            window.location.href = 'index.html'; // Redirecionar para a página de detalhes
+        } else {
+            alert('Senha incorreta');
+        }
     }
-}
-
+    
+    
--- detalhes.js
+++ detalhes.js
@@ -1,48 +1,144 @@
-async function fetchData(url) {
-    const response = await fetch(url);
-    if (!response.ok) {
-        throw new Error('Erro ao buscar dados da API');
-    }
-    return await response.json();
+function atletaIdUrl() {
+    const params = new URLSearchParams(window.location.search);
+    return parseInt(params.get("id"), 10);
 }
 
-async function createCard(data) {
-    const nome = data.name;
-    const posicao = data.position;
-    const imagemUrl = data.image_url;
+async function dadosJogadores(id) {
+    try {
+        const response = await fetch(`https://botafogo-atletas.mange.li/2024-1/${id}`);
+        if (response.ok) {
+            return await response.json();
+        } else {
+            console.error("Erro ao buscar o Jogador:", response.statusText);
+            return null;
+        }
+    } catch (error) {
+        console.error("Erro ao buscar o Jogador:", error);
+        return null;
+    }
+}
 
+const conteudo = document.createElement('div');
+conteudo.style.display = 'flex';
+conteudo.style.flexWrap = 'wrap';
+conteudo.style.justifyContent = 'center';
+conteudo.style.alignItems = 'center';
+conteudo.style.gap = '10px';
+conteudo.innerHTML = 'Loading...';
+
+document.body.appendChild(conteudo);
+
+const montaCard = (entrada) => {
     const card = document.createElement('div');
-    card.classList.add('card');
-
-    const nomeElement = document.createElement('p');
-    nomeElement.textContent = `Nome: ${nome}`;
-
-    const posicaoElement = document.createElement('p');
-    posicaoElement.textContent = `Posição: ${posicao}`;
-
-    const imagemElement = document.createElement('img');
-    imagemElement.src = imagemUrl;
-    imagemElement.alt = 'Imagem do jogador';
-
-    card.appendChild(nomeElement);
-    card.appendChild(posicaoElement);
-    card.appendChild(imagemElement);
-
+    card.style.display = 'grid';
+    card.style.gridTemplateColumns = '1fr 2fr';
+    card.style.backgroundColor = "rgb(0, 97, 60)";
+    card.style.gridTemplateAreas = `
+    "a1 a2"
+    "a1 a3"
+    "a4 a4"
+    "a5 a5"
+    "a6 a6"
+    `;
+    card.style.height = '25rem';
+    card.style.border = 'solid';
+    card.style.padding = '.2rem';
+    
+    const imgContainer = document.createElement('div');
+    imgContainer.style.gridArea = 'a1';
+    imgContainer.style.display = 'flex';
+    imgContainer.style.alignItems = 'center';
+    imgContainer.style.justifyContent = 'center';
+    
+    const imagem = document.createElement('img');
+    imagem.src = entrada.imagem;
+    imagem.alt = `Foto de ${entrada.nome}`;
+    imagem.style.width = '200px';
+    imagem.style.height = '100%';
+    imagem.style.objectFit = 'cover';
+    
+    const posicao = document.createElement('p');
+    posicao.innerHTML = entrada.posicao;
+    posicao.style.cssText = `
+    grid-area: a2;
+    display: flex;
+    text-transform: uppercase;
+    margin: 10px;
+    `;    
+    posicao.style.gridArea = 'a2';
+    posicao.style.display = 'flex';
+    posicao.style.fontWeight = 'bold';
+    posicao.style.textTransform = 'uppercase';
+    
+    const nome = document.createElement('p');
+    nome.innerHTML = entrada.nome;
+    nome.className = 'nome';
+    nome.style.top = '0';
+    nome.style.left = '0';
+    nome.style.margin = '10px';
+    nome.style.gridArea = 'a3';
+    nome.style.display = 'flex';
+    nome.style.gap = "5px";
+    nome.style.fontWeight = 'bold';
+    
+    const descricao = document.createElement('p');
+    descricao.innerHTML = entrada.detalhes;
+    descricao.style.gridArea = 'a4';
+    
+    const nascimento = document.createElement('p');
+    nascimento.innerHTML = entrada.nascimento;
+    nascimento.style.gridArea = 'a5';
+    nascimento.style.top = '0';
+    nascimento.style.left = '0';
+    nascimento.style.margin = '10px';
+    nascimento.style.display = 'flex';
+    nascimento.style.gap = "5px";
+    nascimento.style.fontWeight = 'bold';
+    
+    const voltar = document.createElement('button');
+    voltar.innerHTML = 'Voltar';
+    voltar.style.gridArea = 'a6';
+    voltar.style.backgroundColor = "rgb(255, 255, 255)";
+    voltar.style.fontWeight = 'bold';
+    voltar.style.textAlign = "center";
+    voltar.style.cursor = "pointer";
+    voltar.style.border = 'none';
+    voltar.style.padding = '1px 2px';
+    voltar.style.width = "8.5rem";
+    voltar.style.borderRadius = '5px';
+    voltar.style.fontSize = "1.5em";
+    voltar.style.transition = "transform 0.3s";
+    
+    voltar.classList.add('voltar');
+    voltar.onclick = () => {
+        window.location.href = `index.html`;
+    };
+    
+    card.appendChild(imgContainer);
+    imgContainer.appendChild(imagem);
+    card.appendChild(posicao);
+    card.appendChild(nome);
+    card.appendChild(descricao);
+    card.appendChild(nascimento);
+    card.appendChild(voltar);
+    
     return card;
 }
+if (localStorage.getItem('authorized')){
 
-async function main() {
-    const url = "https://botafogo-atletas.mange.li/2024-1/all"
-
-    const container = document.getElementsByClassName('imagelayout'); // ID do container onde os cards serão adicionados
-
-    try {
-        const data = await fetchData(url);
-        const card = await createCard(data);
-        container.appendChild(card);
-    } catch (error) {
-        console.error(error);
+    
+    (async () => {
+    const atletaId = atletaIdUrl();
+    const atleta = await dadosJogadores(atletaId);
+    if (atleta) {
+        const card = montaCard(atleta);
+        conteudo.innerHTML = ''; 
+        conteudo.appendChild(card);
+    } else {
+        conteudo.innerHTML = 'Erro ao carregar os dados do jogador.';
     }
+})();
 }
-
-main();
+else {
+    window.location.href = "index.html";
+}
